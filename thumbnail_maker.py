1661385020be21a782e9d079e61b560673cf57bc
Okeremeta Jude Tega <okeremeta.tega@gmail.com>
2020-10-22 07:47:50 +0100

    threads, locks and semaphores

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -4,6 +4,7 @@ import os
 import logging
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
+import threading
 
 import PIL
 from PIL import Image
@@ -15,6 +16,23 @@ class ThumbnailMakerService(object):
         self.home_dir = home_dir
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
+        self.downloaded_bytes = 0
+        self.dl_lock = threading.Lock()
+        max_concurrent_dl = 4
+        self.dl_sem = threading.Semaphore(max_concurrent_dl)
+
+    def download_image(self, url):
+        # download each image and save to the input dir 
+        with self.dl_sem:
+            logging.info(f"Downloading image at URL {url}")
+            img_filename = urlparse(url).path.split('/')[-1]
+            dest_path = self.input_dir + os.path.sep + img_filename
+            urlretrieve(url,dest_path)
+            img_size = os.path.getsize(dest_path)
+            with self.dl_lock:
+                self.downloaded_bytes += img_size
+            logging.info(f"Image {img_size} bytes saved to {dest_path}")
+
 
     def download_images(self, img_url_list):
         # validate inputs
@@ -25,10 +43,14 @@ class ThumbnailMakerService(object):
         logging.info("beginning image downloads")
 
         start = time.perf_counter()
+        threads = []
         for url in img_url_list:
-            # download each image and save to the input dir 
-            img_filename = urlparse(url).path.split('/')[-1]
-            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+            t = threading.Thread(target=self.download_image, args=(url,))
+            t.start()
+            threads.append(t)
+        
+        for t in threads:
+            t.join()
         end = time.perf_counter()
 
         logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
@@ -69,7 +91,7 @@ class ThumbnailMakerService(object):
         start = time.perf_counter()
 
         self.download_images(img_url_list)
-        self.perform_resizing()
+        # self.perform_resizing()
 
         end = time.perf_counter()
         logging.info("END make_thumbnails in {} seconds".format(end - start))
