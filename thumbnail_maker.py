0a9020cf52f5d33c2beca78701af19f01188f322
Okeremeta Jude Tega <okeremeta.tega@gmail.com>
2020-10-23 19:45:21 +0100

    asynchronous programming

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -5,12 +5,16 @@ import logging
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
 from queue import Queue
-from threading import Thread
+from threading import Thread, Lock
+import multiprocessing
+import asyncio
+import aiohttp
+import aiofiles
 
 import PIL
 from PIL import Image
 
-FORMAT = "[%(threadName)s, %(asctime)s, %(levelname)s] %(messages)s"
+FORMAT = "[%(threadName)s, %(asctime)s, %(levelname)s] %(message)s"
 logging.basicConfig(filename='logfile.log', level=logging.DEBUG, format=FORMAT)
 
 class ThumbnailMakerService(object):
@@ -18,38 +22,45 @@ class ThumbnailMakerService(object):
         self.home_dir = home_dir
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
-        self.img_queue = Queue()
-        self.dl_queue = Queue()
-        
-    def download_image(self):
-        while not self.dl_queue.empty():
-            try:
-                url = self.dl_queue.get(block=False)
-                img_filename = urlparse(url).path.split('/')[-1]
-                urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-                self.img_queue.put(img_filename)
+        self.img_queue = multiprocessing.JoinableQueue()
+        self.dl_size = 0
+        self.resized_size = multiprocessing.Value('i', 0)
 
-                self.dl_queue.task_done()
-            except Queue.empty:
-                logging.info("Queue empty")
+    async def download_image_coroutine(self, session, url):
+        # download each image and save to the input dir
+        img_filename = urlparse(url).path.split('/')[-1]
+        img_filepath = self.input_dir + os.path.sep + img_filename
+
+        async with session.get(url) as response:
+            async with aiofiles.open(img_filepath, 'wb') as f:
+                content = await response.content.read()
+                await f.write(content)
+
+        self.dl_size += os.path.getsize(img_filepath)
+        self.img_queue.put(img_filename)
+
+    async def download_images_coroutine(self, img_url_list):
+        async with aiohttp.ClientSession() as session:
+            for url in img_url_list:
+                await self.download_image_coroutine(session, url)
+
 
     def download_images(self, img_url_list):
         # validate inputs
         if not img_url_list:
             return
         os.makedirs(self.input_dir, exist_ok=True)
-        
+
         logging.info("beginning image downloads")
 
         start = time.perf_counter()
-        for url in img_url_list:
-            # download each image and save to the input dir 
-            img_filename = urlparse(url).path.split('/')[-1]
-            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-            self.img_queue.put(img_filename)
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self.download_images_coroutine(img_url_list))
+        finally:
+            loop.close()
+        
         end = time.perf_counter()
-
-        self.img_queue.put(None)
 
         logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
 
@@ -59,13 +70,11 @@ class ThumbnailMakerService(object):
 
         logging.info("beginning image resizing")
         target_sizes = [32, 64, 200]
-        num_images = len(os.listdir(self.input_dir))
 
-        start = time.perf_counter()
         while True:
             filename = self.img_queue.get()
             if filename:
-                logging.info(f"resizing image {filename}")
+                logging.info("resizing image {}".format(filename))
                 orig_img = Image.open(self.input_dir + os.path.sep + filename)
                 for basewidth in target_sizes:
                     img = orig_img
@@ -74,44 +83,38 @@ class ThumbnailMakerService(object):
                     hsize = int((float(img.size[1]) * float(wpercent)))
                     # perform resizing
                     img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
-                    
-                    # save the resized image to the output dir with a modified file name 
+
+                    # save the resized image to the output dir with a modified file name
                     new_filename = os.path.splitext(filename)[0] + \
                         '_' + str(basewidth) + os.path.splitext(filename)[1]
-                    img.save(self.output_dir + os.path.sep + new_filename)
+                    out_filepath = self.output_dir + os.path.sep + new_filename
+                    img.save(out_filepath)
+
+                    with self.resized_size.get_lock():
+                        self.resized_size.value += os.path.getsize(out_filepath)
 
                 os.remove(self.input_dir + os.path.sep + filename)
-                logging.info(f"done resizing {filename}")
+                logging.info("done resizing image {}".format(filename))
                 self.img_queue.task_done()
             else:
                 self.img_queue.task_done()
                 break
-        end = time.perf_counter()
-
-        logging.info("created {} thumbnails in {} seconds".format(num_images, end - start))
 
     def make_thumbnails(self, img_url_list):
         logging.info("START make_thumbnails")
         start = time.perf_counter()
 
-        for img_url in img_url_list:
-            self.dl_queue.put(img_url)
+        
+        num_processes = multiprocessing.cpu_count()
+        for _ in range(num_processes):
+            p = multiprocessing.Process(target=self.perform_resizing)
+            p.start()
 
-        num_dl_threads = 4
+        self.download_images(img_url_list)
 
-        for _ in range(num_dl_threads):
-            t1 = Thread(target=self.download_image)
-            t1.start()
-
-
-        t2 = Thread(target=self.perform_resizing)
-
-        t2.start()
-
-        self.dl_queue.join()
-        self.img_queue.put(None)
-        t2.join()
+        for _ in range(num_processes):
+            self.img_queue.put(None)
 
         end = time.perf_counter()
         logging.info("END make_thumbnails in {} seconds".format(end - start))
-    
+        logging.info("Initial size of downloads: [{}]  Final size of images: [{}]".format(self.dl_size, self.resized_size.value))
